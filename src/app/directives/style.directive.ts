78c0dc8316fed964ae2ee31da91551111b3332c5
Olga <Chuchundrium@users.noreply.github.com>
2020-03-14 00:33:41 +0200

    04-Directives-02-elements-get-access

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,12 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-
+  constructor(private el: ElementRef,
+              private r: Renderer2) {
+    this.r.setStyle(el.nativeElement, 'color', 'red'); /* more universal code */
+    // el.nativeElement.style.color = '#306174';                  /* good for browsers only */
+  }
 }
