dafb13c3f383fbc0135c28705dc90c0325ecf849
Olga <chuchundrium@users.noreply.github.com>
2020-03-13 16:48:54 +0200

    09-event-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testProject02';
+  title = 'Dynamic title';
+  inputValue = '';
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
 }
