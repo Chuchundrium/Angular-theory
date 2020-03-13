e68cc81b87b542495e3654e9c53562dca0f278c0
Olga <Chuchundrium@users.noreply.github.com>
2020-03-14 00:47:31 +0200

    04-Directives-03-events-decoratorHostListener

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -6,7 +6,17 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 export class StyleDirective {
   constructor(private el: ElementRef,
               private r: Renderer2) {
-    this.r.setStyle(el.nativeElement, 'color', 'red'); /* more universal code */
+    this.r.setStyle(el.nativeElement, 'color', '#6b5b95'); /* more universal code */
     // el.nativeElement.style.color = '#306174';                  /* good for browsers only */
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event);
+  }
+  @HostListener('mouseenter') onEnter() {
+    this.r.setStyle(this.el.nativeElement, 'color', '#feb236');
+  }
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null);
   }
 }
