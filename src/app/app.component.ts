80fbc9900fda48ec9f446f20df2e86dba9bd2b32
Olga <Chuchundrium@users.noreply.github.com>
2020-03-13 19:10:45 +0200

    16-pipes-what-is-it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arr = [1, 1, 2, 3, 5, 13];
-
-  objs = [
-    {title: 'Post 1', author: 'Chu', comments: [
-        {name: 'Bu', text: 'ta-1'},
-        {name: 'Ru', text: 'ta-2'},
-        {name: 'Ku', text: 'ta-3'},
-      ]},
-    {title: 'Post 2', author: 'ChuChu', comments: [
-        {name: 'Bu', text: 'ta-1'},
-        {name: 'Ru', text: 'ta-2'},
-        {name: 'Ku', text: 'ta-3'},
-      ]},
-  ];
+  now: Date = new Date();
 }
