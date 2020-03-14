214be732f75a7a8f17982cb72acca8a8daa2b0d4
Olga <Chuchundrium@users.noreply.github.com>
2020-03-14 14:23:08 +0200

    05-Pipe-08-filter-posts-by-title-or-text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Banana', text: 'Banana is yellow'},
+    {title: 'Apple', text: 'Apple is red'},
+    {title: 'Pie', text: 'Apple pie with banana'}
+  ];
+
+  search = '';
+  searchField = 'title';
 
 }
