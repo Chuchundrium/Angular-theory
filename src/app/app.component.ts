814016f71d1c73aa4bc1f6ffcd2e312eeb28713f
Olga <Chuchundrium@users.noreply.github.com>
2020-03-14 17:57:54 +0200

    06-Services-03-local-service-providers-in-Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import {AppCounterService} from './services/app-counter.service';
+import {LocalCounterService} from './services/local-counter.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService] // local using of service without import in app.module
 })
 export class AppComponent  {
-  constructor(public appCounterService: AppCounterService) {  }
+  constructor(public appCounterService: AppCounterService,
+              public localCounterService: LocalCounterService) {  }
 
 }
