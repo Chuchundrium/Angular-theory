bdb3c7164549e56ceefd5ebe7c8a75af01558018
Olga <Chuchundrium@users.noreply.github.com>
2020-03-14 17:50:38 +0200

    06-Services-02-decorator-Injectable

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,3 +1,9 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  // for registration in root module without import in providers
+  providedIn: 'root'
+})
 export class AppCounterService {
   counter = 0;
   increase() {
