e3994a7b38d971eca3b4fe6596dc117bead60c5f
Olga <Chuchundrium@users.noreply.github.com>
2020-03-14 12:48:31 +0200

    01-number

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {StyleDirective} from './directives/style.directive';
-import { Style2Directive } from './directives/style2.directive';
-import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StyleDirective,
-    Style2Directive,
-    IfnotDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
