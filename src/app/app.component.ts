91a2d92db0a92716a5694a774cea698f42ede67d
Olga <Chuchundrium@users.noreply.github.com>
2020-03-19 22:13:06 +0200

    08-HttpClient-01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  appState = 'on';
-
-  handleChange() {
-    console.log(this.appState);
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
   }
+
 }
