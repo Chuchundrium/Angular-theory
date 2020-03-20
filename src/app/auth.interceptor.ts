038ac4f6a69d9e7547bc4ef5da44e4974a04bf02
Olga <chuchundrium@users.noreply.github.com>
2020-03-20 17:44:35 +0200

    08-HttpClient-13-RequestModify-clone

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,12 +1,24 @@
 // by the fact, interceptor is a service
 
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 export class AuthInterceptor implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.table('Intercept request', req);
-    return next.handle(req);
-  }
 
+    // we cannot modify req directly! Only clone!
+    const clonedReq = req.clone({
+      headers: req.headers.append('Auth', 'SOME-TOKEN')
+    });
+
+    // this is a stream:
+    return next.handle(clonedReq).pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Interseptor response', event);
+        }
+      })
+    );
+  }
 }
