54e0d7734e23d35216e6b8bc3a1fc8290d2becc4
Olga <Chuchundrium@users.noreply.github.com>
2020-03-21 17:21:42 +0200

    11-additional-01-Dynamic-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { HomePageComponent } from './home-page/home-page.component';
-import {SharedModule} from './shared/shared.module';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  // for registration of: Components, Directives, Pipes
   declarations: [
     AppComponent,
-    HomePageComponent
+    ModalComponent
   ],
-
-  // for other Modules
   imports: [
-
-    // for Angular working in browsers + other base useful things
     BrowserModule,
-
-    // for working with ngModel, ngForms, etc.
     FormsModule,
-
-    // own Modules
-    AppRoutingModule,
-    // for lazy loading:
-    // AboutPageModule,
-    SharedModule
   ],
-
-  // for Services
   providers: [],
-
-  // for main Component in app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
