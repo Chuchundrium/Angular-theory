efbc5ba80e9e462fac1034d44b1a6a2616bc11bb
Olga <Chuchundrium@users.noreply.github.com>
2020-03-21 17:50:30 +0200

    11-additional-02-Dynamic-components

---
+++ src/app/ref.directive.ts
@@ -0,0 +1,12 @@
+// for finding elements in template
+
+import {Directive, ViewContainerRef} from '@angular/core';
+
+@Directive({
+  selector: '[appRef]'
+})
+
+export class RefDirective {
+  constructor(public containerRef: ViewContainerRef) {
+  }
+}
