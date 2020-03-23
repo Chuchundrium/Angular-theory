6eb4a043eafa2283aa77b3dfd711380c72ad2655
Olga <Chuchundrium@users.noreply.github.com>
2020-03-23 20:41:09 +0200

    14-Animation-04-complicated-transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,20 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         background: '#da532c',
         transform: 'scale(1.2)'
       })),
+      state('special', style({
+        background: '#ffc40d',
+        transform: 'scale(0.7)',
+        borderRadius: '50%'
+      })),
       transition('start => end', animate(450)),
-      transition('end => start', animate('800ms ease-in-out'))
+      transition('end => start', animate('800ms ease-in-out')),
+      transition('special <=> *', [
+        style({ background: '#99b433' }),
+        animate('1s', style({
+          background: '#7e3878'
+        })),
+        animate(750)
+      ])
     ])
   ]
 })
