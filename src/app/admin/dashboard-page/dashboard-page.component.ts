3c1f28625deb695b30292bf885fec17b8311b16c
Olga <Chuchundrium@users.noreply.github.com>
2020-03-26 00:44:26 +0200

    15-Practice-Blog-19-search-pipe

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   postsSubscription: Subscription;
+  searchString = '';
 
   constructor(private postsService: PostsService) { }
 
