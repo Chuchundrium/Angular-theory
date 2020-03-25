97d205546044db5f7bc9ff9f1532dec1cb498795
Olga <Chuchundrium@users.noreply.github.com>
2020-03-26 01:07:15 +0200

    15-Practice-Blog-20-post-delete

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   postsSubscription: Subscription;
+  deleteSubscription: Subscription;
   searchString = '';
 
   constructor(private postsService: PostsService) { }
@@ -22,12 +23,18 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     });
   }
 
+  remove(id: string) {
+    this.deleteSubscription = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+    });
+  }
+
   ngOnDestroy(): void {
     if (this.postsSubscription) {
       this.postsSubscription.unsubscribe();
     }
-  }
-
-  remove(id: string) {
+    if (this.deleteSubscription) {
+      this.deleteSubscription.unsubscribe();
+    }
   }
 }
--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -32,6 +32,9 @@ export class PostsService {
           }));
       }));
   }
+  remove(id: string): Observable<void> {
+    return this.http.delete<void>(`${environment.firebaseDatabaseUrl}/posts/${id}.json`);
+  }
 
 
 }
